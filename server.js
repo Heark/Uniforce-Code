0ea9a8a2a5656f1cb79a78673fcedd0df5f42789
Aj <atune@broncos.uncfsu.edu>
2020-05-14 23:35:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,119 @@
-var app = require("express")();
-var PORT = process.env.PORT || 3000;
-console.log(process.env.IP);
-var server = app.listen(PORT, function(){
-  console.log("Server is on");
-});
+var start = new Date().getSeconds()
+const app = require("express")();
+const later = require("later");
+const server = app.listen(3000, function() {
+    var end = new Date().getSeconds()
+    var final = (end - start)
+    var plrl;
+    (final != 1) ? plrl = "s" : plrl = ""
+    console.log("Server Online!")
+    console.log("Server loaded in "+final+" second"+ plrl + ".");
+})
+// https://stackoverflow.com/questions/18536726/javascript-to-display-the-current-date-and-time/18536804
+// Didn't feel like writing my own date and time function, very tedious.
 
-var io = require("socket.io")(server);
-var connectCounter = 0;
-io.on('connection', function(client){
-  console.log(client.id + ' has connected!');
+function formatAMPM() {
+var d = new Date(),
+    minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+    hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+    ampm = d.getHours() >= 12 ? 'pm' : 'am',
+    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+    days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+ampm;
+}
 
-  client.on('hi', function() {
-    client.emit('hi-too');
-  });
+app.get('/', (req, res) => res.send(`Server started at ${formatAMPM()}`))
 
-  client.on('new-connection', function(){
-    connectCounter++;
-  });
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const cors = require('cors')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 
-  client.on('lost-connection', function(){
-    connectCounter--;
-  });
+var upload = require('express-fileupload')
 
-  client.on('get-players-online', function(data){
-    data = connectCounter;
-    client.emit('players-online', data);
-  });
-});
+app.use(upload()).use(cors);
+db.defaults({
+        dailys: [],
+        user: {}
+    })
+    .write()
+
+const io = require("socket.io")(server),
+      dl  = require('delivery'),
+      fs  = require('fs');
+
+var totalPlayers = 0;
+var recieved_bonusCounter = 0;
+var data = 0;
+var the_date = new Date().getHours();
+
+function updateDailyQuest() {
+    console.log('test')
+}
+
+app.get("/save", function(req, res){
+    if(req.files){
+        console.log(req.files)
+    }
+})
+io.on('connection', function(client) {
+    totalPlayers++
+    console.log(client.id + ' Connected!')
+
+    client.on('hi', function() {
+        console.log('hi');
+    })
+
+    client.on('player-connected', function(name) {
+        console.log(name+"connected");
+        client.emit('online-players', totalPlayers);
+    })
+    client.on('new-chat', function(message) {
+        console.log(message);
+        io.emit('got-new-message', message);
+    })
+    client.on('disconnect', function() {
+        totalPlayers--
+    })
+
+    // Daily Quest Begin
+    client.on('daily-start', function(data) {
+        if (db.get('dailys').find({username: data.username}).value()) {
+            db.get('dailys')
+                .find({
+                    username: data.username
+                })
+                .assign({
+                    time: data.time
+                })
+                .write();
+        } else {
+        db.get('dailys')
+            .push(data)
+            .write();
+        }
+    });
+
+    client.on('daily-check', function(data) {
+        var usertime = Date.parse(db.get('dailys').find({
+            username: data.username
+        }).value().time);
+        var currentTime = new Date();
+        var day = 24 * 60 * 60 * 1000;
+        var daysPassed = Math.round(Math.abs((usertime.getTime() - currentTime.getTime()) / (day)));
+
+        if (daysPassed <= 1) {
+            client.emit('daily-checked', true);
+        } else {
+            client.emit('daily-checked', false);
+        }
+
+    })
+
+    client.on('get-daily-quest-id', function(){
+        client.emit('daily-quest-id', 2);
+    })
+
+
+})
